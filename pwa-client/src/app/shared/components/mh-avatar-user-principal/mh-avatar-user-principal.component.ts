2af22c5a248abd605d316b6c21b8cdacd20ded65
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2023-01-23 01:12:02 -0300

    implements get account-data from component

--- pwa-client/src/app/shared/components/mh-avatar-user-principal/mh-avatar-user-principal.component.ts
+++ pwa-client/src/app/shared/components/mh-avatar-user-principal/mh-avatar-user-principal.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
 import { TranslateService } from '@ngx-translate/core';
+import { MhAuthorizationServerClientService } from 'src/app/core/services/http/mh-authorization-server-client.service';
+import { MhUserServiceClientService } from 'src/app/core/services/http/mh-user-service-client.service';
+import { MhAuthorizationHelperService } from 'src/app/core/services/mh-authorization-helper.service';
 import { MhTranslateHelperService } from 'src/app/core/services/mh-translate-helper.service';
+import { AccountData } from '../../models/AccountData.interface';
 
 @Component({
   selector: 'app-mh-avatar-user-principal',
@@ -10,14 +14,27 @@ import { MhTranslateHelperService } from 'src/app/core/services/mh-translate-hel
 })
 export class MhAvatarUserPrincipalComponent implements OnInit {
 
+  private account?: AccountData;
+
   constructor(
     private menuController: MenuController,
     private translate: TranslateService,
-    private translateHelper: MhTranslateHelperService
+    private translateHelper: MhTranslateHelperService,
+    private authorizationHelper: MhAuthorizationHelperService,
+    private authorizationClient: MhAuthorizationServerClientService,
+    private userClient: MhUserServiceClientService
   ) { }
 
   ngOnInit() {
     this.translateHelper.setupTranslateService(this.translate);
+    this.authorizationHelper.getAuthentication()
+      .then((userData) => userData.k2)
+      .then(async (token) => await this.authorizationClient.getDecodedToken(token).toPromise())
+      .then((decodedToken) => {
+        this.userClient.getAccountData(decodedToken.body.sub).subscribe((res)=> {
+          this.account = res.body;
+        });
+      });
   }
 
   closeMenu() {
