a09b6e5983ee8b00f394ee1652ad7eb58c9dafd3
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2023-02-06 01:08:23 -0300

    feature - authorization: verify on Authorization Server if token still valid

--- pwa-client/src/app/core/guards/auth-guard/auth.guard.ts
+++ pwa-client/src/app/core/guards/auth-guard/auth.guard.ts
@@ -1,38 +1,44 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
-import { MhAuthorizationHelperService } from '../../services/mh-authorization-helper.service';
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {Observable} from 'rxjs';
+import {environment} from 'src/environments/environment';
+import {MhAuthorizationHelperService} from '../../services/mh-authorization-helper.service';
+import {MhAuthorizationServerClientService} from '../../services/http/mh-authorization-server-client.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
   constructor(
-    private mhAuthorizationHelperService: MhAuthorizationHelperService
-  ) {}
+    private mhAuthorizationHelperService: MhAuthorizationHelperService,
+    private mhAuthorizationServerClient: MhAuthorizationServerClientService
+  ) {
+  }
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return this.mhAuthorizationHelperService.getAuthentication()
-        .then(auth => {
-          if (!auth) {
-            const clientId = environment.clientId;
-            const url = environment.authorizationServerUrl + '/oauth2/authorize?response_type=code'
-              + '&client_id=' + clientId
-              + '&redirect_uri=' + environment.redirectUrl
-              + '&state=' + 'login'
-              + '&scope=' + 'user:canSelfRead user:canSelfWrite';
+      .then(auth => {
+        if (!auth) {
+          const clientId = environment.clientId;
+          const url = environment.authorizationServerUrl + '/oauth2/authorize?response_type=code'
+            + '&client_id=' + clientId
+            + '&redirect_uri=' + environment.redirectUrl
+            + '&state=' + 'login'
+            + '&scope=' + 'user:canSelfRead user:canSelfWrite';
 
-            window.location.href = encodeURI(url);
-            return false;
+          window.location.href = encodeURI(url);
+          return false;
 
-          } else {
-            return true;
-
-          }
-        });
+        } else {
+          // verify on Authorization Server if token is valid
+          return this.mhAuthorizationServerClient
+            .getDecodedToken(auth.k2)
+            .toPromise()
+            .then(tokenDecoded => tokenDecoded.body.active);
+        }
+      });
   }
 
 }
